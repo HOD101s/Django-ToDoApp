58d424f14dc6fe6b50abdd128b7f51633d5f4d7e
HOD101s <manasbass.99@gmail.com>
2020-06-08 19:08:26 +0530

    models setup

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class MyDo(models.Model):
+	activity = models.CharField(max_length=1000)
+	status = models.BooleanField(default=False)
+	starttimestamp = models.DateTimeField()
+	endtimestamp = models.DateTimeField()
+
+	def __str__(self):
+		return self.activity
